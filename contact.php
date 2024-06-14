36e1737d21b5136e48fc384d9275e38dff567fc9
Alexandre Rossa Rocha <134687348+AlexxandreRossa@users.noreply.github.com>
2024-06-14 18:18:56 +0100

    Add files via upload
    initial commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $mailForm = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "alexxandrerossa@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have a message ".$name".\n\n".$message;
+
+    mail($mailTo, $name, $txt, $headers);
+
+    header("Location: index.html?MessageSent");
+
+}
+
+?>
